24a171607945db2d3336542fd37418dc5e187b9d
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-06 14:37:22 -0400

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Oscar Cristaldo on 2022-07-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
