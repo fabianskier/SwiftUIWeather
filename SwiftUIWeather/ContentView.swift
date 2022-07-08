c5b785a1546c09839c08ab2532f7f887892c84d8
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-08 13:49:05 -0400

    CO-0002: Add dark mode and some code refactoring for iOS 15
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- SwiftUIWeather/ContentView.swift
+++ SwiftUIWeather/ContentView.swift
@@ -8,42 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    var weatherDay: [WeatherDay] = [
+        WeatherDay(dayOfWeek: "TUE",
+                   imageName: "sun.max.fill",
+                   temperature: 32),
+        WeatherDay(dayOfWeek: "WED",
+                   imageName: "cloud.sun.fill",
+                   temperature: 28),
+        WeatherDay(dayOfWeek: "THU",
+                   imageName: "cloud.rain.fill",
+                   temperature: 17),
+        WeatherDay(dayOfWeek: "FRI",
+                   imageName: "cloud.bolt.rain.fill",
+                   temperature: 20),
+        WeatherDay(dayOfWeek: "SAT",
+                   imageName: "cloud.sun.rain.fill",
+                   temperature: 24)
+    ]
+    
     var body: some View {
         ZStack {
-            // Background
-            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(isNight: isNight)
             VStack {
-                // City
-                Text("Asunción, PY")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing: 8) {
-                    // Wheater icon
-                    Image(systemName: "sun.max.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    // Temperature
-                    Text("32°")
-                        .font(.system(size: 70, weight: .medium, design: .default))
-                        .foregroundColor(.white)
-                    
-                }
-                
-                Spacer()
+                CityTextView(cityName: "Asunción, PY")
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "sun.max.fill", temperature: 32)
                 
                 HStack(spacing: 20) {
-                    WheaterDayView(dayOfWeek: "TUE", imageName: "sun.max.fill", temperature: 32)
-                    WheaterDayView(dayOfWeek: "WED", imageName: "cloud.sun.fill", temperature: 28)
-                    WheaterDayView(dayOfWeek: "THU", imageName: "cloud.rain.fill", temperature: 18)
-                    WheaterDayView(dayOfWeek: "FRI", imageName: "cloud.bolt.rain.fill", temperature: 20)
-                    WheaterDayView(dayOfWeek: "SAT", imageName: "cloud.sun.rain.fill", temperature: 24)
+                    // https://www.hackingwithswift.com/forums/swiftui/compiler-warning-non-constant-range-argument-must-be-an-integer-literal/14878
+                    ForEach(0 ..< weatherDay.count, id: \.self) { element in
+                        WeatherDayView(weatherDay: weatherDay[element])
+                    }
                 }
+                
                 Spacer()
                 
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButtonView(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                }
+                
+                Spacer()
             }
         }
     }
@@ -51,29 +59,100 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
 
-struct WheaterDayView: View {
+// MARK: - BackgroundView
+struct BackgroundView: View {
     
-    var dayOfWeek: String
+    var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea(.all)
+    }
+}
+
+// MARK: - CityTextView
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+// MARK: - MainWeatherStatusView
+struct MainWeatherStatusView: View {
+    
     var imageName: String
     var temperature: Int
     
     var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: imageName)
+                .symbolRenderingMode(.multicolor)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+// MARK: - WeatherDayView
+struct WeatherDayView: View {
+    
+    var weatherDay: WeatherDay
+    
+    var body: some View {
         VStack {
-            Text(dayOfWeek)
+            Text(weatherDay.dayOfWeek)
                 .font(.system(size: 16, weight: .medium, design: .default))
                 .foregroundColor(.white)
-            Image(systemName: imageName)
+            Image(systemName: weatherDay.imageName)
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40)
-            Text("\(temperature)°")
+            Text("\(weatherDay.temperature)°")
                 .font(.system(size: 28, weight: .bold, design: .default))
                 .foregroundColor(.white)
         }
     }
+}
+
+// MARK: - WeatherButtonView
+struct WeatherButtonView: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
+
+// MARK: - WeatherDay
+struct WeatherDay {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
 }
